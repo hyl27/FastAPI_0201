1987f7d3bf9ebdd72796f795ddf3e0a54c2e73b0
hyl27 <100427hyl@gmail.com>
2024-02-02 14:15:33 +0900

    2-07-05

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -1,11 +1,13 @@
 <script>
     import fastapi from "../lib/api"
+    import Error from "../components/Error.svelte"
 
     export let params = {}
     let question_id = params.question_id
     //console.log('question_id:'+ question_id)
     let question = {answers:[]}
     let content = ""
+    let error = {detail:[]}
 
     function get_question(){
         fastapi("get", "/api/question/detail/"+question_id, {}, (json) => {
@@ -24,8 +26,12 @@
         fastapi('post', url, params, 
             (json) => {
                 content = ''
+                error = {detail:[]}
                 get_question()
-            }
+            },
+        (err_json) => {
+            error = err_json
+        }
         )
     }
 </script>
@@ -38,6 +44,7 @@
         <li>{answer.content}</li>
     {/each}
 </ul>
+<Error error={error} />
 <form method="post">
     <textarea rows ="15" bind:value={content}></textarea>
     <input type="submit" value="답변등록" on:click="{post_answer}">
