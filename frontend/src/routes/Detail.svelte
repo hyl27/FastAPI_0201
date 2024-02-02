0ff2b22b83955a5c39f2686ea8d6a5974481a7fa
hyl27 <100427hyl@gmail.com>
2024-02-02 12:58:45 +0900

    2-07-01

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -5,6 +5,7 @@
     let question_id = params.question_id
     //console.log('question_id:'+ question_id)
     let question = {}
+    let content = ""
 
     function get_question(){
         fastapi("get", "/api/question/detail/"+question_id, {}, (json) => {
@@ -13,7 +14,26 @@
 }
 
     get_question()
+
+    function post_answer(event) {
+        event.preventDefault()
+        let url = "/api/answer/create/" + question_id
+        let params = {
+            content: content
+        }
+        fastapi('post', url, params, 
+            (json) => {
+                content = ''
+                get_question()
+            }
+        )
+    }
 </script>
 
 <h1>{question.subject}</h1>
 <div> {question.content}</div>
+
+<form method="post">
+    <textarea rows ="15" bind:value={content}></textarea>
+    <input type="submit" value="답변등록" on:click="{post_answer}">
+</form>
