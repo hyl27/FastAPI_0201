c1b208f6efbf2c0ce3be46061f1409d0eb84c53f
hyl27 <100427hyl@gmail.com>
2024-02-02 10:26:45 +0900

    2-06

---
+++ frontend/src/routes/Detail.svelte
@@ -0,0 +1,19 @@
+<script>
+    import fastapi from "../lib/api"
+
+    export let params = {}
+    let question_id = params.question_id
+    //console.log('question_id:'+ question_id)
+    let question = {}
+
+    function get_question(){
+        fastapi("get", "/api/question/detail/"+question_id, {}, (json) => {
+            question = json
+        })
+}
+
+    get_question()
+</script>
+
+<h1>{question.subject}</h1>
+<div> {question.content}</div>
