d8c284157c65fa10d3e7f538d0ffe070261943fd
hyl27 <100427hyl@gmail.com>
2024-02-02 18:03:18 +0900

    3-07-03

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,11 +3,13 @@
   import Home from "./routes/Home.svelte"
   import Detail from "./routes/Detail.svelte"
   import QuestionCreate from "./routes/QuestionCreate.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
 
   const routes = {
     '/': Home,
     '/detail/:question_id': Detail,
     '/question-create': QuestionCreate,
+    '/user-create': UserCreate,
   }
 </script>
 
