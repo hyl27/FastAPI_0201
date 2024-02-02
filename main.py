973a73922dbfeafba763dd8bd0e94bda07803a63
hyl27 <100427hyl@gmail.com>
2024-02-02 17:46:29 +0900

    3-07

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from domain.question import question_router
 from domain.answer import answer_router
+from domain.user import user_router
 
 app = FastAPI()
 
@@ -20,3 +21,5 @@ app.add_middleware(
 
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
+app.include_router(user_router.router)
+
